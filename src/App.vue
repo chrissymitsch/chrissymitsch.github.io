8c842f96f70d40b575014075487375b783efbdda
Chrissy <mitsch.chrissy@gmail.com>
2022-02-24 01:39:13 +0100

    Start work on intro

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+    <nav>Navigation</nav>
+
+    <hello-world></hello-world>
+
+    <main>
+        <article>Introduction</article>
+    </main>
+
+    <section id="profile">
+        <h1>Profile / About Me</h1>
+        <p>I explain myself</p>
+        <video loop width="200" autoplay muted>
+            <source src="media/chocolate-frog.mp4" type="video/mp4">
+        </video>
+    </section>
+
+    <section id="career">
+        <h1>Career</h1>
+    </section>
+
+    <section id="projects">
+        <h1>Projects</h1>
+    </section>
+
+    <footer></footer>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1920px;
+  margin: auto;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,180 @@
+<template>
+  <header>
+      <h2 class="hellooo">Hellooo.</h2>
+      <div class="name">
+        <h1 class="accent">Chrissy</h1>
+        <h1>here.</h1>
+      </div>
+      <div class="whatami">
+        <h3>I'm a</h3>
+        <h2 class="accent">Developer</h2>
+      </div>
+      <div class="code">
+        <p>
+          <strong>if</strong>
+          youWantToKnowMore(aboutMe)
+        </p>
+        <p>
+          <strong>then</strong>
+          scrollDown
+        </p>
+        <p>
+          <strong>endif</strong>
+        </p>
+      </div>
+      <figure class="bridge"><img src="./../assets/img/steg.png" /></figure>
+  </header>
+</template>
+
+<style scoped>
+header {
+  min-height: 100vh;
+  overflow: hidden;
+}
+
+.bridge {
+  bottom: -20vh;
+  width: 100vh;
+  max-width: 100vw;
+  max-height: 100%;
+  position: absolute;
+}
+
+.bridge img {
+  width: 100%;
+}
+
+h1, h2, h3 {
+  display: inline-block;
+  line-height: 100%;
+}
+
+h1 {
+  font-weight: 700;
+  font-size: 13.5vh;
+}
+
+h2 {
+  font-weight: 700;
+  font-size: 8vh;
+}
+
+h3 {
+  position: absolute;
+  font-weight: 700;
+  font-size: 6vh;
+}
+
+.hellooo {
+  position: absolute;
+  left: 10%;
+  top: 20vh;
+}
+
+.name {
+  left: 17%;
+  top: 25vh;
+  margin-top: 5vh;
+}
+
+.name h1 {
+  margin-left: 1.5vh;
+  margin-right: 1.5vh;
+}
+
+.whatami {
+  position: absolute;
+  right: 15%;
+  top: 45vh;
+  margin-top: 5vh;
+}
+
+.whatami .accent {
+  font-size: 9vh;
+  top: 8vh;
+}
+
+.code {
+  font-size: 3vh;
+  position: absolute;
+  right: 6%;
+  top: 70vh;
+}
+
+.code p {
+  line-height: 120%;
+}
+
+.code strong {
+  color: var(--color-text-highlight);
+}
+
+@media (max-width: 1200px) { /* 1024 */
+  .bridge {
+    bottom: -17vh;
+  }
+
+  h1 {
+    font-size: 10vh;
+  }
+
+  h2 {
+    font-size: 6vh;
+  }
+
+  h3 {
+    font-size: 4vh;
+  }
+
+  .whatami .accent {
+    font-size: 7vh;
+  }
+}
+
+@media (max-width: 920px) { /* 768 */
+  header {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-flow: column;
+  }
+
+  h3, .hellooo, .whatami {
+    position: relative;
+  }
+
+  .hellooo {
+    margin-top: 5vh;
+  }
+
+  .hellooo, .name, .whatami, .whatami .accent {
+    left: inherit;
+    top: inherit;
+    right: inherit;
+    bottom: inherit;
+    text-align: center;
+    display: block;
+  }
+
+  .bridge {
+    width: 140%;
+    left: -20%;
+  }
+
+  .bridge img {
+    width: 140%;
+  }
+}
+
+@media (max-width: 480px) {
+  .bridge {
+    bottom: -10vh;
+    width: 160%;
+    left: -30%;
+  }
+
+  .bridge img {
+    width: 160%;
+  }
+}
+</style>
