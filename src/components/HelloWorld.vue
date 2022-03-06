c2ae4349efb7c4505e84d3dd7f6789fd91a34978
Chrissy <mitsch.chrissy@gmail.com>
2022-03-06 13:12:57 +0100

    start work on mobile first

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -30,91 +30,85 @@
 
 <style scoped>
 header {
-  min-height: 100vh;
-  overflow: hidden;
+  min-height: calc(100vh - 4rem);
+  max-width: 1024px;
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin: auto;
+  margin-top: 4rem;
+  row-gap: 5vh;
 }
+
 .bridge {
   position: absolute;
   left: 0;
-  bottom: -20vh;
+  bottom: min(calc(100vmax - 150vmin), 0%);
   width: 100vh;
   max-width: 100vw;
   max-height: 100%;
 }
 
 h1, h2, h3 {
+  font-weight: 700;
   display: inline-block;
   line-height: 100%;
 }
 
 h1 {
-  font-weight: 700;
   font-size: max(min(8rem, 13.5vmin), 2rem);
 }
 
 h2 {
-  font-weight: 700;
   font-size: max(min(4rem, 8vmin), 1rem);
 }
 
 h3 {
-  position: absolute;
-  font-weight: 700;
   font-size: max(min(3rem, 6vmin), 1rem);
 }
 
 .hellooo {
-  position: absolute;
-  left: max(0%, calc(30vw - 40vh));
-  top: min(20vh, 20vw);
   opacity: 0;
   animation: fadeIn 2s ease-out 0.25s forwards;
 }
 
 .name {
-  left: max(0%, calc(25vw - 15vh));
-  top: min(25vh, 25vw);
-  margin-top: 5vh;
-  transform: translateX(-5vw);
+  display: flex;
+  align-items: baseline;
+  justify-content: center;
   opacity: 0;
+  transform: translateX(-5vw);
   animation: rollIn 1s ease-out 0.5s forwards, fadeIn 2s ease-out 0.5s forwards;
 }
 
-.hellooo, .name h1, .whatami, .code {
-  margin-left: 1.5vmin;
-  margin-right: 1.5vmin;
-}
-
 .whatami {
-  position: absolute;
-  left: max(0%, calc(75vw - 47vh));
-  top: max(45vmin, 10rem);
-  margin-top: 5vh;
-  width: max(50vw, 100vmin);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
 }
 
 .whatami h3 {
-  transform: translateY(-5vh);
   opacity: 0;
+  transform: translateY(-5vh);
   animation: rollDown 1s ease-out 1s forwards, fadeIn 1s ease-out 1s forwards;
 }
 
 .whatami .accent {
   font-size: max(min(6rem, 10vmin), 2rem);
-  top: 3rem;
+  opacity: 0;
   transform-origin: top center;
   transform: scale(0.7);
-  opacity: 0;
   animation: zoomIn 1s ease-out 1.5s forwards, fadeIn 1s ease-out 1.5s forwards;
 }
 
 .code {
-  font-size: max(min(1.2rem, 5vmin), 1rem);
   position: absolute;
-  left: max(0%, calc(100vw - 60vh));
-  bottom: max(15vmin, 0rem);
-  transform: translateY(5vh);
+  bottom: 10vh;
+  font-size: max(min(1.2rem, 5vmin), 1rem);
   opacity: 0;
+  transform: translateY(5vh);
   animation: rollUp 1s ease-out 2s forwards, fadeIn 1s ease-out 2s forwards;
 }
 
@@ -127,10 +121,15 @@ h3 {
   color: var(--color-text-highlight);
 }
 
+.hellooo, .name h1, .whatami, .code {
+  margin-left: 1.5vmin;
+  margin-right: 1.5vmin;
+}
+
 .wave-front, .wave-back {
   position: absolute;
-  bottom: -2px;
-  width: 100%;
+  bottom: 0;
+  width: 100vw;
   height: 30vh;
   background-position: top center;
   background-size: cover;
@@ -147,63 +146,6 @@ h3 {
 
 /* Responsiveness */
 
-@media (max-width: 1200px) { /* 1024 */
-  .bridge {
-    bottom: -17vh;
-  }
-}
-
-@media (max-aspect-ratio: 4/3) {
-  header {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    flex-flow: column;
-  }
-
-  h3, .hellooo, .whatami {
-    position: relative;
-  }
-
-  .hellooo {
-    margin-top: 5vh;
-  }
-
-  .hellooo, .name, .whatami, .whatami .accent {
-    left: inherit;
-    top: inherit;
-    right: inherit;
-    bottom: inherit;
-    text-align: center;
-    display: block;
-  }
-
-  .bridge {
-    width: 140%;
-    left: -50%;
-    bottom: -20vh;
-  }
-
-  .bridge img {
-    width: 140%;
-  }
-
-  .code {
-    display: none;
-  }
-}
-
-@media (max-aspect-ratio: 9/15) {
-  .bridge {
-    bottom: -34vh;
-    left: -39vh;
-    width: 100vh;
-  }
-
-  .bridge img {
-    width: 100vh;
-  }
-}
 
 @keyframes fadeIn {
   0% {
--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -8,6 +8,7 @@
 <style scoped>
 main {
   min-height: 100vh;
+  max-width: 100vw;
   background-color: var(--color-background);
 }
 </style>
